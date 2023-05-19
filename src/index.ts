71ff67a5e542d73569d031c5516210cc36914ce4
emon-webdev <emon.webdev@gmail.com>
2023-05-19 12:01:08 +0600

    set basic express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('first')
+
+import mongoose from 'mongoose';
+import app from './app';
+
+const port: Number = 5000
+
+
+
+//database connetion
+async function bootstrap() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
+        console.log(`Database connection successfully`)
+
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`)
+        })
+
+    } catch (err) {
+        console.log("Fail to connect database", err)
+    }
+}
+bootstrap()
+
+
